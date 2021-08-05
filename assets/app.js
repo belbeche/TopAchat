0128bfe851633d351d9177f4618fe5a4ad5b3493
= <wbelbeche.s@gmail.com>
2021-08-05 14:21:30 +0200

    adding new template

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,18 @@
 
 // Template app
 import './styles/bootstrap.min.css';
-import './styles/simplex.min.css';
 
-// TopAchat Layout
-import './styles/card.css';
-import './styles/app.css';
 // import './styles/mini.css';
+
+/* Css application
+import './styles/font-awesome.min.css';
+import './styles/elegant-icons.css';
+import './styles/nice-select.css';
+import './styles/magnific-popup.css';
+import './styles/jquery-ui.min.css';
+import './styles/owl.carousel.min.css';
+import './styles/slicknav.min.css';
+import './styles/style.css';*/
 
 
 // start the Stimulus application
